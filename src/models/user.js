d66e47fccee23a1837fc699003b419d8d4977785
howard-rose <philip.anthon@gmail.com>
2023-08-06 23:56:46 +0800

    implemented password hashing

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,7 @@
 import { Schema, SchemaTypes, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema({
     username: {
@@ -15,6 +18,28 @@ const userSchema = new Schema({
         ref: 'profile',
         require: true
     }
+});
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password'))
+        return next();
+
+    try {
+        // Generate salt
+        const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+        // Generate hash
+        const hash = await bcrypt.hash(this.password, salt);
+        // Replace password with hash
+        this.password = hash;
+        next();
+    } catch (err) {
+        console.error(err);
+        return next(err);
+    }
+});
+
+userSchema.method('comparePassword', function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
 });
 
 export const User = model('user', userSchema);
