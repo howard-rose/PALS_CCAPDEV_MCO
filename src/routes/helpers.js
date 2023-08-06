706e3928f99a1a67377a38ec84ea3468b124afd2
howard-rose <philip.anthon@gmail.com>
2023-08-07 06:50:08 +0800

    voting on posts part 2

--- src/routes/helpers.js
+++ src/routes/helpers.js
@@ -2,6 +2,7 @@ import { Post } from '../models/post.js';
 import { Comment } from '../models/comment.js';
 import { PostVote } from '../models/post_vote.js';
 import { CommentVote } from '../models/comment_vote.js';
+import { User } from '../models/user.js';
 
 export const getScorePost = async (id) => {
     return await PostVote.countDocuments({post: id, score: 1}).lean().exec() - await PostVote.countDocuments({post: id, score: -1}).lean().exec();
@@ -11,35 +12,45 @@ export const getScoreComment = async (id) => {
     return await CommentVote.countDocuments({comment: id, score: 1}).lean().exec() - await PostVote.countDocuments({comment: id, score: -1}).lean().exec();
 }
 
-export async function loadPostById(id) {
+export async function loadPostById(id, current_user) {
     const currPost = await Post.findById(id).populate('user').populate('comments').lean().exec();
-    await loadPostScores(currPost);
+    await loadPostScores(currPost, current_user);
 
     return currPost;
 }
 
-export async function loadPosts(filter) {
+export async function loadPosts(filter, current_user) {
     const posts = await Post.find(filter).populate('user').lean().exec();
 
     for (const currPost of posts) {
-        await loadPostScores(currPost);
+        await loadPostScores(currPost, current_user);
     }
 
     return posts;
 }
 
-export async function loadComments(filter) {
+export async function loadComments(filter, current_user) {
     const comments = await Comment.find(filter).populate('user').lean().exec();
 
     for (const comment of comments) {
-        await loadCommentScores(comment);
+        await loadCommentScores(comment, current_user);
     }
 
     return comments;
 }
 
-export async function loadPostScores(post) {
+export async function loadPostScores(post, current_user) {
     post.score = await getScorePost(post._id);
+    
+    post.current_user_vote = 0;
+    if (current_user) {
+        const user_id = await User.findOne({username: current_user}, '_id').lean().exec();
+        const postVote = await PostVote.findOne({user: user_id, post: post._id}, 'score').lean().exec();
+
+        if (postVote) {
+            post.current_user_vote = postVote.score;
+        }
+    }
     //console.log(`Score of post: ${post.score}`);
 
     for (const comment of post.comments) {
@@ -47,8 +58,18 @@ export async function loadPostScores(post) {
     }
 }
 
-export async function loadCommentScores(comment) {
+export async function loadCommentScores(comment, current_user) {
     comment.score = await getScoreComment(comment._id);
+
+    comment.current_user_vote = 0;
+    if (current_user) {
+        const user_id = await User.findOne({username: current_user}, '_id').lean().exec();
+        const commentVote = await CommentVote.findOne({user: user_id, comment: comment._id}, 'score').lean().exec();
+        
+        if (commentVote) {
+            comment.current_user_vote = commentVote.score;
+        }
+    }
 
     if (comment.comments) {
         for (const subcomment of comment.comments) {
@@ -56,3 +77,18 @@ export async function loadCommentScores(comment) {
         }
     }
 }
+
+/*
+export async function getUserPostVote(user_filter, post_filter) {
+    const user_id = await User.findOne(user_filter, '_id').lean().exec();
+    const post_id = await Post.findOne(post_filter, '_id').lean().exec();
+
+    return await PostVote.findOne({user: user_id, post: post_id}, 'score').lean().exec();
+}
+
+export async function getUserCommentVote(user_filter, comment_filter) {
+    const user_id = await User.findOne(user_filter, '_id').lean().exec();
+    const comment_id = await Comment.findOne(comment_filter, '_id').lean().exec();
+
+    return await CommentVote.findOne({user: user_id, comment: comment_id}, 'score').lean().exec();
+}*/
